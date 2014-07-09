f717c5d3748897d607c52f9a5f65d5c845c50de5
Ryo Sakamoto <rsakamot.0620@gmail.com>
2014-07-10 06:27:59 +0900

    fixed epel

--- scripts/base.sh
+++ scripts/base.sh
@@ -2,8 +2,7 @@
 
 sed -i "s/^.*requiretty/#Defaults requiretty/" /etc/sudoers
 
-rpm -i "/Users/rsakamoto/Downloads/2ping-2.0-2.el6.noarch.rpm"
-#rpm -i https://dl.fedoraproject.org/pub/epel/6/x86_64/2ping-2.0-2.el6.noarch.rpm
+#rpm -i  http://ftp-srv2.kddilabs.jp/Linux/distributions/fedora/epel/6/x86_64/epel-release-6-8.noarch.rpm
 
 yum -y install gcc make gcc-c++ kernel-devel-`uname -r` zlib-devel openssl-devel readline-devel sqlite-devel perl wget
 yum -y install curl bind-utils file git mailx man ntp openssh-clients patch rsync screen sysstat dstat htop traceroute vim-enhanced
